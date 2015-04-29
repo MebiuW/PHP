2cf81b217890211e19835b0eeac3431e6863293e
MebiuW <727705424@qq.com>
2015-04-29 18:00:30 +0800

    Demo+
    Demo+

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php 
+return array(
+	'DB_TYPE'			=>	'mysql',
+	'DB_HOST'			=>	'localhost',
+	'DB_NAME'   => 'thinkphp', // 数据库名
+	'DB_USER'			=>	'root',		//数据库用户名	
+	'DB_PWD'			=>	'',//数据库登录密码
+	'DB_PORT'			=>	'3306',
+	'DB_PREFIX'		=>	'think_',//数据库表名前缀
+);
+?>
